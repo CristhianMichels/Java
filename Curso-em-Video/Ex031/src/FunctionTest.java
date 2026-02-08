98d7ac8095591b82f9c8f7ece75b2c9480da1a27
Cristhian Michels <cristhianpostmichelscpm@gmail.com>
2026-02-08 14:47:13 -0300

    Methods

---
+++ Curso-em-Video/Ex031/src/FunctionTest.java
@@ -0,0 +1,12 @@
+public class FunctionTest {
+
+    static int soma(int a, int b){
+       int s = a + b;
+       return s;
+    }
+
+    public static void main(String[] args) {
+        int sm = soma(5,2);
+        System.out.println(sm);
+    }
+}
---
+++ Curso-em-Video/Ex032/src/FunctionTest/Main.java
@@ -0,0 +1,8 @@
+package FunctionTest;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("The countdown is about to begin.");
+        System.out.println(Operacoes.contador(1,5));
+    }
+}
